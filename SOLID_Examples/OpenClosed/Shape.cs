a0b1b4735e1cee2373fe5d0c81222e3b1c1446b0
Ibrahim <ibrahimasadov32@gmail.com>
2023-07-28 15:57:39 +0400

    commit

---
+++ SOLID_Examples/OpenClosed/Shape.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Examples.OpenClosed
+{
+
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class AreaCalculator
+    {
+        public double CalculateTotalArea(Shape[] shapes)
+        {
+            double totalArea = 0;
+            foreach (var shape in shapes)
+            {
+                totalArea += shape.Area();
+            }
+            return totalArea;
+        }
+    }
+
+}
+
---
+++ SOLID_Examples/SingleResponsiblity/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Examples.SingleResponsiblity
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+    public class CustomerRepository
+    {
+        public void SaveCustomer(Customer customer)
+        {
+            Console.WriteLine($"Customer:{customer.Name} {customer.Address}");
+        }
+    }
+    public class EmailService
+    {
+        public void SendWelcomeEmail(Customer customer)
+        {
+            Console.WriteLine($"Sending welcome email to {customer.Name} at {customer.Address}");
+        }
+
+    }
+}
